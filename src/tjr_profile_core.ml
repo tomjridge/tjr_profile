bd1013ca868bf2633b1bdc7570ab417cdb36d86c
Tom Ridge <tom.j.ridge@googlemail.com>
2019-07-05 18:13:12 +0100

    +

--- src/tjr_profile_core.ml
+++ src/tjr_profile_core.ml
@@ -37,6 +37,16 @@ let dummy_profiler = {
 
 let _ = dummy_profiler
 
+(** This is a profiler that fails if any method is called; useful for
+   checking that all profilers have been initialized (even if only to
+   dummy_profiler) *)
+let failing_profiler = {
+  mark=(fun _ -> failwith (Printf.sprintf "%s: mark\n%s" __MODULE__ __LOC__));
+  get_marks=(fun _ -> failwith (Printf.sprintf "%s: get_marks\n%s" __MODULE__ __LOC__));
+  print_summary=(fun () ->
+    failwith (Printf.sprintf "%s: print_summary\n%s" __MODULE__ __LOC__))      
+}
+  
 
-(** A reference to a dummy profiler *)
+(** A reference to a dummy profiler, for quick and dirty single-profiler use *)
 let string_profiler : string profiler ref = ref dummy_profiler
