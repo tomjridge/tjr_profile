c98b561eefc514a9efcbb289aba4607343e86dfa
Tom Ridge <tom.j.ridge@googlemail.com>
2019-07-11 17:48:35 +0100

    +

--- src_with_core/tjr_profile_with_core.ml
+++ src_with_core/tjr_profile_with_core.ml
@@ -2,7 +2,10 @@
 
 (** NOTE: this library implicitly sets
    {!Tjr_profile.now} to use Core.Time_stamp_counter by
-   default *)
+   default.
+
+NOTE do not open this module - Make_profiler clashes with a similar functor in {!Tjr_fs_shared}.
+ *)
 
 let now = Core.(Time_stamp_counter.(fun () ->
   now () 
@@ -13,3 +16,21 @@ let now = Core.(Time_stamp_counter.(fun () ->
 let initialize () = 
   (* Printf.printf "Initializing Tjr_profile.now\n"; *)
   Tjr_profile.now := Some now
+
+let _ = initialize()
+
+(** Make profiling functions profile and mark, without indirecting via
+   an object. Note also that this functor ignores whether profiling is
+   actually enabled or not via ppx_optcomp *)
+module Make_profiler() = struct
+  let internal_profiler = Tjr_profile.make_string_profiler()
+  let mark = internal_profiler.mark
+  let profile s f = mark s; f() |> fun r -> mark (s^"'"); r
+end
+
+module Internal = Make_profiler()
+
+let time_function s f =
+  Internal.internal_profiler.time_function s f 
+
+let _ = time_function
