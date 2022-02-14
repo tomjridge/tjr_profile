6e2811301650972b947c071639b0f765f2ab17ae
Tom Ridge <tom.j.ridge@googlemail.com>
2022-02-14 18:13:57 +0000

    Sketch out new interfaces

---
+++ src/proposed_intf.ml
@@ -0,0 +1,7 @@
+(** Proposed interface *)
+
+(** *)
+
+(* Let's say that we have a default interface that uses mtime, mtime.clock and
+   mtime.clock.os (ignoring jsoo for now). Then we also have an interface that uses JS
+   TSC as the timer. *)
